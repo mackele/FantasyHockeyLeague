5c017834b8e5644cb354dd485a7fb5db87f3efac
mackele <larsmarcus.lennartsson@gmail.com>
2022-04-05 08:32:09 +0200

    Gjort routes för alla HTML filer och strukturerat

--- fhl.py
+++ fhl.py
@@ -10,6 +10,46 @@ FHL = Flask(__name__)
 def index():
     return render_template('index.html')
 
+@FHL.route('/buy-players/')
+def buy_players():
+    return render_template('buy_players.html')
+
+@FHL.route('/forum/')
+def forum():
+    return render_template('forum.html')
+
+@FHL.route('/guide/')
+def guide():
+    return render_template('guide.html')
+
+@FHL.route('/login/')
+def login():
+    return render_template('login.html')
+
+@FHL.route('/match/')
+def match():
+    return render_template('match.html')
+
+@FHL.route('/matchhistorik/')
+def match_history():
+    return render_template('matchhistory.html')
+
+@FHL.route('/mina-spelare/')
+def my_players():
+    return render_template('my_players.html')
+
+@FHL.route('/registrera/')
+def registration():
+    return render_template('registration.html')
+
+@FHL.route('/top-spelare')
+def top_scorer():
+    return render_template('topscorer.html')
+
+@FHL.route('/inlägg/')
+def write_post():
+    return render_template('write_post.html')
+
 
 #Server
 FHL.run(host="127.0.0.1", port=8080, debug=True)
