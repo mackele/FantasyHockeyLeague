2435b8410c47e858a03a454b021fa2d7e74d07ac
Lukas Jönsson <94693238+LukasJnsson@users.noreply.github.com>
2022-04-06 11:51:49 +0200

    Added comments to the routes + sorted
    Added comments to the routes and sorted them in chronological order. (From a user-end perspective).

--- fhl.py
+++ fhl.py
@@ -6,46 +6,67 @@ from flask import Flask, render_template, redirect, url_for, request, redirect
 FHL = Flask(__name__)
 
 
+#Index 
 @FHL.route('/')
 def index():
     return render_template('index.html')
 
-@FHL.route('/köp-spelare/')
-def buy_players():
-    return render_template('buy_players.html')
 
-@FHL.route('/forum/')
-def forum():
-    return render_template('forum.html')
-
-@FHL.route('/guide/')
-def guide():
-    return render_template('guide.html')
-
+#Sign in
 @FHL.route('/login/')
 def login():
     return render_template('login.html')
 
-@FHL.route('/match/')
-def match():
-    return render_template('match.html')
 
-@FHL.route('/match-historik/')
-def match_history():
-    return render_template('matchhistory.html')
-
-@FHL.route('/mina-spelare/')
-def my_players():
-    return render_template('my_players.html')
-
+#Sign up
 @FHL.route('/registrera/')
 def registration():
     return render_template('registration.html')
 
+
+#Guide
+@FHL.route('/guide/')
+def guide():
+    return render_template('guide.html')
+
+
+#Buy players
+@FHL.route('/köp-spelare/')
+def buy_players():
+    return render_template('buy_players.html')
+
+
+#My players
+@FHL.route('/mina-spelare/')
+def my_players():
+    return render_template('my_players.html')
+
+
+#Game
+@FHL.route('/match/')
+def match():
+    return render_template('match.html')
+
+
+#Game history
+@FHL.route('/match-historik/')
+def match_history():
+    return render_template('matchhistory.html')
+
+
+#Toplist
 @FHL.route('/top-spelare')
 def top_scorer():
     return render_template('topscorer.html')
 
+
+#Forum
+@FHL.route('/forum/')
+def forum():
+    return render_template('forum.html')
+
+
+#Forum posts
 @FHL.route('/inlägg/')
 def write_post():
     return render_template('write_post.html')
