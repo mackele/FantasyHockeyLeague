ffb1c18919c19bd774fc28b21c7e127c5e62b9fa
Lukas Jönsson <94693238+LukasJnsson@users.noreply.github.com>
2022-04-06 17:10:39 +0200

    Updated
    Imported modules to enable database connectivity.
    Created function to INSERT FHL-Form post (HTML → Python → PostgreSQL).
    PostgreSQL attributes and HTML labels now have the same names.

--- fhl.py
+++ fhl.py
@@ -1,6 +1,7 @@
 #Imported modules 
 from flask import Flask, render_template, redirect, url_for, request, redirect
-
+import psycopg2
+from psycopg2 import Error
 
 #Application 
 FHL = Flask(__name__)
@@ -70,6 +71,53 @@ def forum():
 @FHL.route('/inlägg/')
 def write_post():
     return render_template('write_post.html')
+
+
+@FHL.route('/form', methods=['POST'])
+def form():
+    """
+    Function inserts post to database
+    """
+
+    #Connect to FHL Database
+    try: 
+        connection = psycopg2.connect(user="grupp2_onlinestore", 
+        password="n8siil4c",
+        host="pgserver.mau.se",
+        port="5432",
+        database="grupp2_onlinestore")
+        cursor = connection.cursor()
+
+        #In fhl_forum add variable fhl_user (foreign key to fhl_user(user_name))
+
+        #Need to add .get in order to function as variable and INSERT to database
+        title = request.form.get("title")
+        category = request.form.get("category")
+        text = request.form.get("text")
+        likes = 21 
+
+        PostgreSQL_insert = """ INSERT INTO forum_test (title, category, text, likes) VALUES (%s, %s, %s, %s)"""
+        insert_to = (title, category, text, likes)
+        cursor.execute(PostgreSQL_insert, insert_to)
+
+        connection.commit()
+        count = cursor.rowcount
+
+
+    except (Exception, Error) as error:
+        print("Error while connectin to FHL Database", error)
+        
+
+    #Close connection to FHL Database
+    finally:
+        if connection:
+            cursor.close()
+            connection.close()
+            
+            #Redirect
+            return render_template('forum.html')
+
+
 
 
 #Server
