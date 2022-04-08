d36488bce6d06af44fa16e2c82af858a856a02a8
emilia-joha <emilia.joha@live.se>
2022-04-08 09:58:33 +0200

    Update login.py

--- login.py
+++ login.py
@@ -1,2 +1,11 @@
-import flask
+from flask import Flask, render_template, redirect, url_for, request, redirect
+import psycopg2
+from psycopg2 import Error
+from datetime import date
+from datetime import datetime
 from  flask_login 
+
+
+#Application 
+FHL = Flask(__name__)
+
