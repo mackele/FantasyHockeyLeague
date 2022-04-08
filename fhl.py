7ee66c3cd8b33b5d23e3aa3531e0af8cf2014799
Lukas Jönsson <94693238+LukasJnsson@users.noreply.github.com>
2022-04-08 14:48:46 +0200

    Updates
    Prepared functions for connect- and disconnect from database
    New function with the purpose to allow the user to select forum post within a category (Not done)
    Minor changes in indentation and added some features on HTML templates (forum.html, write_post.html)

--- fhl.py
+++ fhl.py
@@ -11,7 +11,17 @@ FHL = Flask(__name__)
 
 
 #SQL connect (Create function)
+def fhl_database_connect():
+    """
+    Function allows user to connect to database
+    """ 
+
+
 #SQL disconnect (Create function)
+def fhl_database_disconnect():
+    """
+    Function allows user to disconnect from database
+    """
 
 #SQL-SELECT connect to route (Change)
 connection = psycopg2.connect(user="grupp2_onlinestore", 
@@ -150,7 +160,49 @@ def form():
             return render_template('forum.html', fhldata=data)
 
 
+@FHL.route('/category', methods=['POST','GET'])
+def get_form():
+    """
+    Function displays posts in selected category 
+    """
 
+    #Connect to FHL Database
+    try: 
+        connection = psycopg2.connect(user="grupp2_onlinestore", 
+        password="n8siil4c",
+        host="pgserver.mau.se",
+        port="5432",
+        database="grupp2_onlinestore")
+        cursor = connection.cursor()
+        
+        #User select  
+        category = 'player'
+        #request.get.category('category')
+        #cursor.execute (f""" SELECT * from fhl_forum_form where category = '{category}'; """)
+        #data = cursor.fetchall()
+        #return render_template('forum.html', fhldata=data)
+        
+
+        cursor.execute (f""" SELECT * from fhl_forum_form where category = '{category}'; """)
+        fhldata = cursor.fetchall()
+
+        for data in fhldata:
+            print("{:<25}{:<25}{:<25}{:<25}{:<25}{:<25}".format(data[0], data[1], data[2], data[3], data[4], data[5], data[6]))
+        print("-"*150)
+        cursor.close()
+
+
+    except (Exception, Error) as error:
+        print("Error while connectin to FHL Database", error)
+        
+
+    #Close connection to FHL Database
+    finally:
+        if connection:
+            cursor.close()
+            connection.close()
+
+get_form()
 
 #Server
 FHL.run(host="127.0.0.1", port=8080, debug=True)
