43cfd816c0a0d546b7df8dc7559042160a3ecf05
emilia-joha <emilia.joha@live.se>
2022-04-08 10:16:32 +0200

    Update login.py

--- login.py
+++ login.py
@@ -3,9 +3,19 @@ import psycopg2
 from psycopg2 import Error
 from datetime import date
 from datetime import datetime
-from  flask_login 
+import database
+import  flask_login 
+
 
 
 #Application 
 FHL = Flask(__name__)
+FHL.secret_key='hej' #ändra senare!
 
+login_manager=flask_login.loginManager()
+login_manager.init_app (FHL)
+
+
+@FHL.route('/login/')
+def login():
+    return render_template('login.html')
