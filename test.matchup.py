0d4d7bd8d0dd5184ccd1cfd2d8ff87512190d678
Alexander <95655020+ellestrom@users.noreply.github.com>
2022-04-13 15:24:37 +0200

    Create test.matchup.py

---
+++ test.matchup.py
@@ -0,0 +1,59 @@
+from pstats import Stats
+import requests
+import numpy as np 
+import json
+import pandas as pd
+import math
+
+def get():
+    playerid = "8479291"
+    seasonid = "20202021"
+    team_input = "10"
+
+    # Request
+    url = "https://statsapi.web.nhl.com/api/v1/teams/" + str(team_input) + "?expand=team.roster&season=" + seasonid
+    response = requests.get(url)
+    data = response.json()
+
+    data = data['teams'][0]['roster']['roster']
+
+    team = {}
+
+    var = []
+
+    for i in data:
+        playerId = i['person']["id"]
+        playerName = i['person']['fullName']
+        team[playerName] = playerId
+
+    stats = {"Name": ["Goals", "Assists", "Points"]}
+
+    for i in team:
+        leaf = team[i]
+        r = requests.get('https://statsapi.web.nhl.com/api/v1/people/' + str(leaf) + '/stats?stats=statsSingleSeason&season=' + seasonid)
+        data = json.loads(r.text)
+
+        # Checks if there is points or assists, won't include players without points (rookies, goalies, etc)
+        if len(data['stats'][0]['splits']) > 0:
+            try:
+                goals = data['stats'][0]['splits'][0]['stat']['goals']
+            except:
+                goals = 0
+            try:
+                assists = data['stats'][0]['splits'][0]['stat']['assists']
+            except:
+                assists = 0
+            try:
+                points = data['stats'][0]['splits'][0]['stat']['points']
+            except:
+                points = 0
+            stats[i] = [playerName, goals, assists, points]
+        else:
+            stats[i] = [0, 0, 0]
+
+        var.append(stats[i])
+
+    print(var)
+
+
+get()
